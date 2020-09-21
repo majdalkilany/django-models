cb3bf525f00d0963b2df21aa91cd6a5dbeb65a5e
majdalkilany <majdkilany92@gmail.com>
2020-09-21 18:31:08 +0300

    lab_27

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView, DetailView 
+from .models import Post
 
 # Create your views here.
+
+class HomeView(ListView):
+    template_name = 'home.html'
+    model = Post
+
+class PostDetales(DetailView):
+    template_name = 'post_detail.html'
+    model = Post
